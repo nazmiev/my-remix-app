fc08afc3a003ac15618d59d54135134c52429b58
Роман Назмиев <roma.nazmiev@gmail.com>
2023-05-19 14:47:04 +0700

    Привет, Андрей!

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,7 +18,7 @@ export function headers({
 export default function Index() {
   return (
     <main style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Приятного аппетита!</h1>
       <ul>
         <li>
           <a
